8ad7b881a1a5973150105d6c4841177c7f5914bd
Ruslan <mutygullin@yandexlyceum.ru>
2023-12-27 12:33:02 +0400

    added filters cheatsheet

--- women/urls.py
+++ women/urls.py
@@ -7,8 +7,12 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
+
+    path('template_filters_cheatsheet/', views.template_filters_cheatsheet, name='template_filters_cheatsheet'),
+
     path('cats/<int:cat_id>/', views.categories, name='cats_id'),
     path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+
     # re_path(r'^archive/(?P<year>[0-9]{4})', views.archive) => converters.py
     path('archive/<year4:year>', views.archive, name='archive'),
 ]
--- women/views.py
+++ women/views.py
@@ -6,11 +6,29 @@ from django.urls import reverse
 
 
 def index(request: HttpRequest):
-    return render(request, 'women/index.html')
+    return render(request, 'women/index.html', {'title': 'Главная страница'})
+
+
+def template_filters_cheatsheet(request):
+    class MyClass:
+        def __init__(self, a, b):
+            self.a, self.b = a, b
+
+    menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+    context = {
+        'title': 'примеры использования фильтров в шаблонах',
+        'menu': menu,
+        'float': 42.1,
+        'lst': [1, 2, 3],
+        'set': {4, 5, 6},
+        'dict': {'key': 'value', 'key2': 42},
+        'object': MyClass(1, 2),
+    }
+    return render(request, 'women/template_filters_cheatsheet.html', context)
 
 
 def about(request: HttpRequest):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request: HttpRequest, cat_id):
