c0f63f63e6424f8b533e06350f8339c99f29aa9d
Ruslan <mutygullin@yandexlyceum.ru>
2023-12-27 11:04:34 +0400

    added a converter and used a different converters

--- women/urls.py
+++ women/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
-from . import views
+from . import views, converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories),
-
+    path('cats/<int:cat_id>/', views.categories),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
+    # re_path(r'^archive/(?P<year>[0-9]{4})', views.archive) => converters.py
+    path('archive/<year4:year>', views.archive),
 ]
 
--- women/views.py
+++ women/views.py
@@ -6,5 +6,13 @@ def index(request: HttpRequest):
     return HttpResponse('Страница приложения women.')
 
 
-def categories(request: HttpRequest):
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def categories(request: HttpRequest, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>ID: {cat_id}')
+
+
+def categories_by_slug(request: HttpRequest, cat_slug):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>Slug: {cat_slug}')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Архив по годам</h1><p>Год: {year}')
